7479e4197d049c2d9734a7902164f8563350f634
liuwenwu <651427460@qq.com>
2019-12-24 16:30:44 +0800

    测试

--- authentication_parent/authentication_manage_demo/src/main/java/com/liuww/auth/ManageApplication.java
+++ authentication_parent/authentication_manage_demo/src/main/java/com/liuww/auth/ManageApplication.java
@@ -14,6 +14,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 public class ManageApplication {
     public static void main(String[] args) {
+		// ≤‚ ‘≤‚ ‘≤‚ ‘
         SpringApplication.run(ManageApplication.class,args);
     }
 }
